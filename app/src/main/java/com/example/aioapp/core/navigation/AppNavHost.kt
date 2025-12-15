f3382a7953ec7e5e6777b1424eda90874d05108c
Lautaro Budin <lautaro_budin@hotmail.com>
2025-12-15 15:11:02 -0300

    Implement initial FileManagerScreen

--- app/src/main/java/com/example/aioapp/core/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/aioapp/core/navigation/AppNavHost.kt
@@ -5,6 +5,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.aioapp.ui.home.HomeScreen
+import com.example.aioapp.ui.filemanager.FileManagerScreen
 
 @Composable
 fun AppNavHost() {
@@ -23,7 +24,7 @@ fun AppNavHost() {
         }
 
         composable("filemanager") {
-
+            FileManagerScreen()
         }
     }
 }
