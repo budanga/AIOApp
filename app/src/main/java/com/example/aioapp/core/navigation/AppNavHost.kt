d8caa379f9cc961b7c58490fb76b7c27774a3bcd
Lautaro Budin <lautaro_budin@hotmail.com>
2026-02-26 00:18:21 -0300

    New AppNavHost from branch master

--- app/src/main/java/com/example/aioapp/core/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/aioapp/core/navigation/AppNavHost.kt
@@ -1,30 +1,36 @@
 package com.example.aioapp.core.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.example.aioapp.ui.home.HomeScreen
+import com.example.aioapp.MainViewModel
 import com.example.aioapp.ui.filemanager.FileManagerScreen
+import com.example.aioapp.ui.home.HomeScreen
+import com.example.aioapp.ui.notes.NotesScreen
+import com.example.aioapp.ui.settings.SettingsScreen
 
 @Composable
-fun AppNavHost() {
-    val navController = rememberNavController()
-
+fun AppNavHost(navController: NavHostController, viewModel: MainViewModel, padding: PaddingValues) {
     NavHost(
         navController = navController,
         startDestination = "home"
     ) {
         composable("home") {
-            HomeScreen(
-                onFeatureClick = {
-                    navController.navigate(it)
-                }
-            )
+            HomeScreen(padding = padding, navController = navController)
         }
 
         composable("filemanager") {
-            FileManagerScreen()
+            FileManagerScreen(navController = navController)
+        }
+
+        composable("notes") {
+            NotesScreen()
+        }
+
+        composable("settings") {
+            SettingsScreen(viewModel = viewModel, padding = padding)
         }
     }
 }
