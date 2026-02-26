84271eaeb16403420b56893d971cd85fba370cce
Lautaro Budin <lautaro_budin@hotmail.com>
2026-02-26 00:42:19 -0300

    Add initial NotesScreen just for functionality

---
+++ app/src/main/java/com/example/aioapp/ui/notes/NotesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.aioapp.ui.notes
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NotesScreen() {
+    Text(text = "Notes Screen")
+}
