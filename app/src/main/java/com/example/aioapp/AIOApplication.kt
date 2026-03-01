86bec7b5f73afdbfb6bf172f1f5c273f6700185a
Lautaro Budin <lautaro_budin@hotmail.com>
2026-03-01 12:04:59 -0300

    Remove unused import

--- app/src/main/java/com/example/aioapp/AIOApplication.kt
+++ app/src/main/java/com/example/aioapp/AIOApplication.kt
@@ -3,7 +3,6 @@ package com.example.aioapp
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
